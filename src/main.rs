f0a2bdca823a06307360e4b48a221207591f44b7
Alexandru Geana <ab@geana.io>
2021-11-10 21:47:27 +0100

    use probe-run i/o openocd and rtt i/o uarte

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,21 @@
 #![no_std]
 #![no_main]
 
-use core::fmt::Write;
-use hal::{gpio, uarte, uarte::Uarte};
 use nrf52840_hal as hal;
+
+use rtt_target::{rprintln, rtt_init_print};
 
 #[cortex_m_rt::entry]
 fn main() -> ! {
-    let p = hal::pac::Peripherals::take().unwrap();
-    let port0 = hal::gpio::p0::Parts::new(p.P0);
-
-    let cdc_pins = uarte::Pins {
-        txd: port0
-            .p0_15
-            .into_push_pull_output(gpio::Level::High)
-            .degrade(),
-        rxd: port0.p0_20.into_floating_input().degrade(),
-        cts: None,
-        rts: None,
-    };
-
-    let mut uarte = Uarte::new(
-        p.UARTE0,
-        cdc_pins,
-        uarte::Parity::EXCLUDED,
-        uarte::Baudrate::BAUD115200,
-    );
-
-    write!(uarte, "Hello, World!\r\n").unwrap();
+    rtt_init_print!();
+    rprintln!("Hello world!");
 
     loop {
         cortex_m::asm::wfi();
     }
 }
 
-#[panic_handler] // panicking behavior
+#[panic_handler]
 fn panic(_: &core::panic::PanicInfo) -> ! {
     loop {
         cortex_m::asm::bkpt();
