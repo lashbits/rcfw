313e591f8721554301802abbd5dbd9cae8d80ef1
Alexandru Geana <ab@geana.io>
2021-11-16 19:27:56 +0100

    compiles and links with softdevice

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,60 @@
 #![no_std]
 #![no_main]
+#![feature(alloc_error_handler)]
 
 use nrf52840_hal as hal;
+use nrf_softdevice_defmt_rtt as _; // global logger
 
-use rtt_target::{rprintln, rtt_init_print};
+use alloc_cortex_m::CortexMHeap;
+use core::alloc::Layout;
+use core::mem;
+use core::sync::atomic::{AtomicUsize, Ordering};
+use defmt::panic;
+use nrf_softdevice::ble::central;
+use nrf_softdevice::raw;
+use nrf_softdevice::Softdevice;
+
+// this is the allocator the application will use
+#[global_allocator]
+static ALLOCATOR: CortexMHeap = CortexMHeap::empty();
 
 #[cortex_m_rt::entry]
 fn main() -> ! {
-    rtt_init_print!();
-    rprintln!("Hello world!");
+    let config = nrf_softdevice::Config {
+        clock: Some(raw::nrf_clock_lf_cfg_t {
+            source: raw::NRF_CLOCK_LF_SRC_RC as u8,
+            rc_ctiv: 4,
+            rc_temp_ctiv: 2,
+            accuracy: 7,
+        }),
+        conn_gap: Some(raw::ble_gap_conn_cfg_t {
+            conn_count: 6,
+            event_length: 6,
+        }),
+        conn_gatt: Some(raw::ble_gatt_conn_cfg_t { att_mtu: 128 }),
+        gatts_attr_tab_size: Some(raw::ble_gatts_cfg_attr_tab_size_t {
+            attr_tab_size: 32768,
+        }),
+        gap_role_count: Some(raw::ble_gap_cfg_role_count_t {
+            adv_set_count: 1,
+            periph_role_count: 3,
+            central_role_count: 3,
+            central_sec_count: 0,
+            _bitfield_1: raw::ble_gap_cfg_role_count_t::new_bitfield_1(0),
+        }),
+        gap_device_name: Some(raw::ble_gap_cfg_device_name_t {
+            p_value: b"HelloRust" as *const u8 as _,
+            current_len: 9,
+            max_len: 9,
+            write_perm: unsafe { mem::zeroed() },
+            _bitfield_1: raw::ble_gap_cfg_device_name_t::new_bitfield_1(
+                raw::BLE_GATTS_VLOC_STACK as u8,
+            ),
+        }),
+        ..Default::default()
+    };
+
+    Softdevice::enable(&config);
 
     loop {
         cortex_m::asm::wfi();
@@ -20,4 +66,10 @@ fn panic(_: &core::panic::PanicInfo) -> ! {
     loop {
         cortex_m::asm::bkpt();
     }
+}
+
+// define what happens in an Out Of Memory (OOM) condition
+#[alloc_error_handler]
+fn alloc_error(_layout: Layout) -> ! {
+    panic!("Alloc error");
 }
